3fbd2756e9a924d0b4b2ae242f4a6e508f811ac7
ayushaks999 <ayushaks999@gmail.com>
2025-05-22 13:25:22 +0530

    Add files via upload

---
+++ Voice_Assistant/main.py
@@ -0,0 +1,12 @@
+from app import app  # Import the Flask app from app.py
+import logging
+
+if __name__ == "__main__":
+    host = "127.0.0.1"  # Define the host
+    port = 5006        # Define the port
+
+    print(f"Starting Flask app on http://{host}:{port}")
+    logging.info(f"Starting Flask app on http://{host}:{port}")
+
+    # Run the Flask app
+    app.run(host=host, port=port, debug=True)
